0d2bc80a0ffeacfa671cac6eac6caf17833b0ae9
Dmitry Zheshinsky <demonotter@gmail.com>
2017-03-02 20:14:23 +0600

    Updated tag

--- PublisherApp/PublisherApp.h
+++ PublisherApp/PublisherApp.h
@@ -18,7 +18,6 @@ FOUNDATION_EXPORT double PublisherAppVersionNumber;
 FOUNDATION_EXPORT const unsigned char PublisherAppVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <PublisherApp/PublicHeader.h>
-#import "AFNetworking.h"
-#import "ReactiveCocoa.h"
+#import <AFNetworking/AFNetworking.h>
 
 
